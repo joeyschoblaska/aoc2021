c3d104a14bb940f4256ae678bf1654e3e6d975f8
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-11-30 23:35:38 -0600

    1b

--- 1a.rb
+++ 1a.rb
@@ -1,12 +1,12 @@
 incs = 0
 prev = nil
 
-File.open("1a.txt") do |file|
-  while cur = file.gets
-    cur = cur.to_i
+File
+  .open("1a.txt")
+  .each_line do |line|
+    cur = line.to_i
     incs += 1 if prev && cur > prev
     prev = cur
   end
-end
 
 p incs
---
+++ 1b.rb
@@ -0,0 +1,13 @@
+incs = 0
+buff = []
+
+File
+  .open("1a.txt")
+  .each_line do |line|
+    buff << line.to_i
+    next unless buff.count == 4
+    incs += 1 if buff[0, 3].sum < buff[1, 3].sum
+    buff.shift
+  end
+
+p incs
