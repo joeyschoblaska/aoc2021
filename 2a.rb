d7df9cce721a6c551d7883e9e8310b9d5e5b314c
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-02 09:45:56 -0600

    use IO::foreach

--- 2a.rb
+++ 2a.rb
@@ -1,13 +1,10 @@
 pos = [0, 0]
-
 dirs = { "up" => [0, -1], "down" => [0, 1], "forward" => [1, 0] }
 
-File
-  .open("2.txt")
-  .each_line do |line|
-    dir, dist = line.split(" ")
-    velo = dirs[dir].map { |d| d * dist.to_i }
-    pos = pos.each_with_index.map { |c, i| c + velo[i] }
-  end
+File.foreach("2.txt") do |line|
+  dir, dist = line.split(" ")
+  velo = dirs[dir].map { |d| d * dist.to_i }
+  pos = pos.each_with_index.map { |c, i| c + velo[i] }
+end
 
 p pos[0] * pos[1]
--- 2b.rb
+++ 2b.rb
@@ -1,19 +1,17 @@
 pos = [0, 0]
 aim = 0
 
-File
-  .open("2.txt")
-  .each_line do |line|
-    dir, x = line.split(" ")
+File.foreach("2.txt") do |line|
+  dir, x = line.split(" ")
 
-    case dir
-    when "up"
-      aim -= x.to_i
-    when "down"
-      aim += x.to_i
-    when "forward"
-      pos = [pos[0] + x.to_i, pos[1] + aim * x.to_i]
-    end
+  case dir
+  when "up"
+    aim -= x.to_i
+  when "down"
+    aim += x.to_i
+  when "forward"
+    pos = [pos[0] + x.to_i, pos[1] + aim * x.to_i]
   end
+end
 
 p pos[0] * pos[1]
