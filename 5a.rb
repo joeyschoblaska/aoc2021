e97d97081420eb6fe18bc2cf92cf81f6cb854b96
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-04 23:20:20 -0600

    5

---
+++ 5a.rb
@@ -0,0 +1,18 @@
+coords = Hash.new(0)
+
+File.foreach("5.txt", chomp: true) do |line|
+  x1, y1, x2, y2 = line.scan(/\d+/).map(&:to_i)
+
+  next unless x1 == x2 || y1 == y2
+
+  loc = [x1, y1]
+  dir = [[[x2 - x1, -1].max, 1].min, [[y2 - y1, -1].max, 1].min]
+
+  loop do
+    coords[loc] += 1
+    break if loc == [x2, y2]
+    loc = [loc[0] + dir[0], loc[1] + dir[1]]
+  end
+end
+
+p coords.values.select { |v| v > 1 }.count
