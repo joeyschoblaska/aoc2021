92c8176720620ed1ad8a14cebdaf5fe4422f9b8a
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-06 14:25:35 -0600

    🐟

--- 6/62.rb
+++ 6/62.rb
@@ -1,5 +1,4 @@
 input = File.read("6/6.txt")
-feesh = (0..8).map { |i| input.count(i.to_s) }
-256.times { feesh.rotate! && feesh[6] += feesh[8] }
-
-p feesh.sum
+🐟 = (0..8).map { |i| input.count(i.to_s) }
+256.times { 🐟.rotate! && 🐟[6] += 🐟[8] }
+p 🐟.sum
