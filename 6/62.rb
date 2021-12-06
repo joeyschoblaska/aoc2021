6a1b8196aba982ce8707ff7c84e5417db893d320
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-06 08:46:32 -0600

    rotate

--- 6/62.rb
+++ 6/62.rb
@@ -1,8 +1,8 @@
 input = File.read("6/6.txt")
-feesh = (0..9).map { |i| input.count(i.to_s) }
+feesh = (0..8).map { |i| input.count(i.to_s) }
 
 256.times do
-  feesh[8] = feesh.shift
+  feesh.rotate!
   feesh[6] += feesh[8]
 end
 
