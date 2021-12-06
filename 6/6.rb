fb9e0082337eaf5bc1e44a24e367fea43f29b5ba
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-05 23:59:40 -0600

    human-readable variables

--- 6/6.rb
+++ 6/6.rb
@@ -4,12 +4,12 @@ File.read("6/6.txt").split(",").each { |f| feesh[f.to_i] += 1 }
 
 256.times do
   feesh =
-    feesh.each_with_object(Hash.new(0)) do |(k, v), h|
-      if k == 0
-        h[6] += v
-        h[8] += v
+    feesh.each_with_object(Hash.new(0)) do |(days_to_breed, count), h|
+      if days_to_breed == 0
+        h[6] += count
+        h[8] += count
       else
-        h[k - 1] += v
+        h[days_to_breed - 1] += count
       end
     end
 end
