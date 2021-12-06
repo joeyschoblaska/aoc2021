7030708cf6300c3350f14ff66c7e905b50a21f33
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-06 08:48:20 -0600

    line golf

--- 6/62.rb
+++ 6/62.rb
@@ -1,9 +1,5 @@
 input = File.read("6/6.txt")
 feesh = (0..8).map { |i| input.count(i.to_s) }
-
-256.times do
-  feesh.rotate!
-  feesh[6] += feesh[8]
-end
+256.times { feesh.rotate! && feesh[6] += feesh[8] }
 
 p feesh.sum
