13f14b30a6c4389726e4dfde81d0cc5ad9d64c69
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-10 05:11:41 -0600

    chars

--- 10/10a.rb
+++ 10/10a.rb
@@ -1,24 +1,22 @@
 require_relative "array"
 
 points = { ")" => 3, "]" => 57, "}" => 1197, ">" => 25_137 }
-brackets = "(){}<>[]".split(//)
+brackets = "(){}<>[]".chars
 score = 0
 
 File.foreach("10/10.txt", chomp: true) do |line|
   stack = []
-  line
-    .split(//)
-    .each do |ch|
-      if brackets.index(ch).even?
-        stack << ch
-      elsif brackets.before(ch) == stack.last
-        stack.pop
-      else
-        puts "#{line} - Expected #{brackets.after(stack.last)}, got #{ch}"
-        score += points[ch]
-        break
-      end
+  line.chars.each do |ch|
+    if brackets.index(ch).even?
+      stack << ch
+    elsif brackets.before(ch) == stack.last
+      stack.pop
+    else
+      puts "#{line} - Expected #{brackets.after(stack.last)}, got #{ch}"
+      score += points[ch]
+      break
     end
+  end
 end
 
 p score
--- 10/10b.rb
+++ 10/10b.rb
@@ -1,7 +1,7 @@
 require_relative "array"
 
 points = { ")" => 1, "]" => 2, "}" => 3, ">" => 4 }
-brackets = "(){}<>[]".split(//)
+brackets = "(){}<>[]".chars
 scores = []
 
 File.foreach("10/10.txt", chomp: true) do |line|
@@ -9,18 +9,16 @@ File.foreach("10/10.txt", chomp: true) do |line|
   score = 0
   error = false
 
-  line
-    .split(//)
-    .each do |ch|
-      if brackets.index(ch).even?
-        stack << ch
-      elsif brackets.before(ch) == stack.last
-        stack.pop
-      else
-        error = true
-        break
-      end
+  line.chars.each do |ch|
+    if brackets.index(ch).even?
+      stack << ch
+    elsif brackets.before(ch) == stack.last
+      stack.pop
+    else
+      error = true
+      break
     end
+  end
 
   next if error
   next if stack.empty?
