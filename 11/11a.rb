2898797c7771d4bdb57f65e12028063e83f4c882
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-11 17:04:59 -0600

    🐙

--- 11/11a.rb
+++ 11/11a.rb
@@ -1,32 +1,32 @@
-@octos = {}
+@🐙 = {}
 flashes = 0
 
 File
   .readlines("11/input.txt", chomp: true)
   .each_with_index do |row, y|
-    row.chars.each_with_index { |c, x| @octos[[x, y]] = c.to_i }
+    row.chars.each_with_index { |c, x| @🐙[[x, y]] = c.to_i }
   end
 
 def neighbors(x, y)
   [x - 1, x, x + 1].map { |xx| [y - 1, y, y + 1].map { |yy| [xx, yy] } }
     .flatten(1)
-    .select { |xy| @octos[xy] && xy != [x, y] }
+    .select { |xy| @🐙[xy] && xy != [x, y] }
 end
 
 def flash(x, y)
-  @octos[[x, y]] += 1
+  @🐙[[x, y]] += 1
 
   neighbors(x, y).each do |xy|
-    @octos[xy] += 1 unless @octos[xy] == 10
-    flash(*xy) if @octos[xy] == 10
+    @🐙[xy] += 1 unless @🐙[xy] == 10
+    flash(*xy) if @🐙[xy] == 10
   end
 end
 
 100.times do
-  @octos.each { |k, v| @octos[k] += 1 }
-  @octos.each { |k, v| flash(*k) if v == 10 }
-  flashes += @octos.select { |k, v| v > 9 }.count
-  @octos.each { |k, v| @octos[k] = 0 if v > 9 }
+  @🐙.each { |k, v| @🐙[k] += 1 }
+  @🐙.each { |k, v| flash(*k) if v == 10 }
+  flashes += @🐙.select { |k, v| v > 9 }.count
+  @🐙.each { |k, v| @🐙[k] = 0 if v > 9 }
 end
 
 p flashes
--- 11/11b.rb
+++ 11/11b.rb
@@ -1,34 +1,34 @@
-@octos = {}
+@🐙 = {}
 
 File
   .readlines("11/input.txt", chomp: true)
   .each_with_index do |row, y|
-    row.chars.each_with_index { |c, x| @octos[[x, y]] = c.to_i }
+    row.chars.each_with_index { |c, x| @🐙[[x, y]] = c.to_i }
   end
 
 def neighbors(x, y)
   [x - 1, x, x + 1].map { |xx| [y - 1, y, y + 1].map { |yy| [xx, yy] } }
     .flatten(1)
-    .select { |xy| @octos[xy] && xy != [x, y] }
+    .select { |xy| @🐙[xy] && xy != [x, y] }
 end
 
 def flash(x, y)
-  @octos[[x, y]] += 1
+  @🐙[[x, y]] += 1
 
   neighbors(x, y).each do |xy|
-    @octos[xy] += 1 unless @octos[xy] == 10
-    flash(*xy) if @octos[xy] == 10
+    @🐙[xy] += 1 unless @🐙[xy] == 10
+    flash(*xy) if @🐙[xy] == 10
   end
 end
 
 1.step do |i|
-  @octos.each { |k, v| @octos[k] += 1 }
-  @octos.each { |k, v| flash(*k) if v == 10 }
+  @🐙.each { |k, v| @🐙[k] += 1 }
+  @🐙.each { |k, v| flash(*k) if v == 10 }
 
-  if @octos.select { |k, v| v > 9 }.count == 100
+  if @🐙.select { |k, v| v > 9 }.count == 100
     puts "everybody flash your hands! #{i.to_s}"
     break
   end
 
-  @octos.each { |k, v| @octos[k] = 0 if v > 9 }
+  @🐙.each { |k, v| @🐙[k] = 0 if v > 9 }
 end
