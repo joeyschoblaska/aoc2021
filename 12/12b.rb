1ef08d6b123c657d9d94ab5e548a3da98d99e040
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-12 09:46:36 -0600

    speed up

--- 12/12b.rb
+++ 12/12b.rb
@@ -9,6 +9,10 @@ File.foreach("12/input.txt", chomp: true) do |line|
   @exits[b] += [a]
 end
 
+def visited_small_cave_twice?(path)
+  path.uniq.any? { |p| p.small? && path.count(p) == 2 }
+end
+
 def spelunk(cave, path = [])
   path = path + [cave]
 
@@ -19,10 +23,7 @@ def spelunk(cave, path = [])
   else
     @exits[cave].each do |c|
       next if c == "start"
-
-      doubled = path.uniq.any? { |p| p.small? && path.count(p) == 2 }
-
-      next if c.small? && doubled && path.include?(c)
+      next if c.small? && path.include?(c) && visited_small_cave_twice?(path)
 
       spelunk(c, path)
     end
