93f86c4e52ff67708e10341688a64cfa3c21c50d
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-13 13:23:01 -0600

    grid

--- 13/13b.rb
+++ 13/13b.rb
@@ -1,11 +1,12 @@
-paper = []
+require "./lib/grid"
+
+paper = Grid.new
 folds = []
 
 File.foreach("13/input.txt", chomp: true) do |line|
   if line =~ /\d+,\d+/
     x, y = line.split(",").map(&:to_i)
-    paper[x] ||= []
-    paper[x][y] = 1
+    paper[x, y] = 1
   elsif line =~ /fold/
     dir, loc = line[/[xy]=\d+/].split("=")
     folds << [dir, loc.to_i]
@@ -13,29 +14,22 @@ File.foreach("13/input.txt", chomp: true) do |line|
 end
 
 folds.each do |folddir, foldloc|
-  folded = []
+  folded = Grid.new
 
-  paper.each_with_index do |col, x|
-    next unless col
+  paper.each do |(x, y), val|
+    next if folddir == "x" && x == foldloc
+    next if folddir == "y" && y == foldloc
 
-    col.each_with_index do |e, y|
-      next if folddir == "x" && x == foldloc
-      next if folddir == "y" && y == foldloc
-
-      if folddir == "x" && x > foldloc
-        folded[foldloc - (x - foldloc)] ||= []
-        folded[foldloc - (x - foldloc)][y] ||= e
-      elsif folddir == "y" && y > foldloc
-        folded[x] ||= []
-        folded[x][foldloc - (y - foldloc)] ||= e
-      else
-        folded[x] ||= []
-        folded[x][y] = e
-      end
+    if folddir == "x" && x > foldloc
+      folded[foldloc - (x - foldloc), y] ||= val
+    elsif folddir == "y" && y > foldloc
+      folded[x, foldloc - (y - foldloc)] ||= val
+    else
+      folded[x, y] = val
     end
   end
 
   paper = folded
 end
 
-paper.reverse.each { |col| puts (col || []).map { |e| e ? "#" : " " }.join }
+paper.each_row { |row| puts row.map { |e| e ? "#" : " " }.join }
---
+++ lib/grid.rb
@@ -0,0 +1,58 @@
+class Grid
+  include Enumerable
+
+  def initialize()
+    @rows = []
+  end
+
+  def []=(x, y, val)
+    @rows[y] ||= []
+    @rows[y][x] = val
+  end
+
+  def <<(row)
+    @rows << row
+  end
+
+  def [](x, y)
+    @rows[y][x]
+  rescue NoMethodError
+    nil
+  end
+
+  def each
+    return if @rows.empty?
+    (0..max_x).each { |x| (0..max_y).each { |y| yield [[x, y], self[x, y]] } }
+  end
+
+  def max_x
+    @rows.map { |r| r&.count || 0 }.max - 1
+  end
+
+  def max_y
+    @rows.count - 1
+  end
+
+  def trim!
+    @rows.pop until @rows[-1] && @rows[-1].any?
+    @rows.each { |row| row.pop until row[-1] }
+  end
+
+  def inspect
+    (0..max_x)
+      .map { |x| (0..max_y).map { |y| self[x, y].inspect }.join(", ") }
+      .join("\n")
+  end
+
+  def to_s
+    inspect
+  end
+
+  def each_row
+    (0..max_y).each { |y| yield (0..max_x).map { |x| self[x, y] } }
+  end
+
+  def each_col
+    (0..max_x).each { |x| yield (0..max_y).map { |y| self[x, y] } }
+  end
+end
