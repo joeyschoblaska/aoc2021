cfecc5c08cfc2d42259dd2cf224b6bd752caae98
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-21 10:50:38 -0600

    golf

--- 21/21b.rb
+++ 21/21b.rb
@@ -1,35 +1,27 @@
 @memo = {}
 
-def play(p1pos, p1score, p2pos, p2score, to_move = 1, roll = 1, rolled = 0)
-  @memo[[p1pos, p1score, p2pos, p2score, to_move, roll, rolled]] ||=
-    if p1score >= 21
+def play(p1, p2, to_move = 1, roll = 1, rolled = 0)
+  @memo[[p1, p2, to_move, roll, rolled]] ||=
+    if p1[1] >= 21
       [1, 0]
-    elsif p2score >= 21
+    elsif p2[1] >= 21
       [0, 1]
     else
       [1, 2, 3].map do |die|
           if roll < 3
-            play(
-              p1pos,
-              p1score,
-              p2pos,
-              p2score,
-              to_move,
-              roll + 1,
-              rolled + die
-            )
+            play(p1, p2, to_move, roll + 1, rolled + die)
           elsif to_move == 1
-            pos = p1pos + die + rolled
+            pos = p1[0] + die + rolled
             pos -= 10 if pos > 10
-            play(pos, p1score + pos, p2pos, p2score, 2)
+            play([pos, p1[1] + pos], p2, 2)
           else
-            pos = p2pos + die + rolled
+            pos = p2[0] + die + rolled
             pos -= 10 if pos > 10
-            play(p1pos, p1score, pos, p2score + pos, 1)
+            play(p1, [pos, p2[1] + pos], 1)
           end
         end
         .reduce([0, 0]) { |acc, a| [acc[0] + a[0], acc[1] + a[1]] }
     end
 end
 
-p play(5, 0, 9, 0).max
+p play([5, 0], [9, 0]).max
