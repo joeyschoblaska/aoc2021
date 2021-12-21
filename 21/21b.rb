15b4e9804e547ca41d5305f56bf99d9a4b1d2c3c
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-21 10:55:15 -0600

    clarity

--- 21/21b.rb
+++ 21/21b.rb
@@ -2,26 +2,26 @@
 
 def play(p1, p2, to_move = 1, roll = 1, rolled = 0)
   @memo[[p1, p2, to_move, roll, rolled]] ||=
-    if p1[1] >= 21
+    if p1[:score] >= 21
       [1, 0]
-    elsif p2[1] >= 21
+    elsif p2[:score] >= 21
       [0, 1]
     else
       [1, 2, 3].map do |die|
           if roll < 3
             play(p1, p2, to_move, roll + 1, rolled + die)
           elsif to_move == 1
-            pos = p1[0] + die + rolled
+            pos = p1[:pos] + die + rolled
             pos -= 10 if pos > 10
-            play([pos, p1[1] + pos], p2, 2)
+            play({ pos: pos, score: p1[:score] + pos }, p2, 2)
           else
-            pos = p2[0] + die + rolled
+            pos = p2[:pos] + die + rolled
             pos -= 10 if pos > 10
-            play(p1, [pos, p2[1] + pos], 1)
+            play(p1, { pos: pos, score: p2[:score] + pos }, 1)
           end
         end
         .reduce([0, 0]) { |acc, a| [acc[0] + a[0], acc[1] + a[1]] }
     end
 end
 
-p play([5, 0], [9, 0]).max
+p play({ score: 0, pos: 5 }, { score: 0, pos: 9 }).max
