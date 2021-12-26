fd47f1f10ff2208da5fbeb5f6f70f756ef382c5c
Joey Schoblaska <joey.schoblaska@gmail.com>
2021-12-26 15:30:52 -0600

    24!

--- 24/24.rb
+++ 24/24.rb
@@ -1,8 +1,19 @@
 require_relative "execute"
-require "pry"
+require "json"
 
 def test(i, w, z)
   execute(@instruction_sets[i], [w], 0, 0, 0, z)[3]
 end
 
-binding.pry
+candidates = JSON.parse(File.read("24/candidates.json"))
+zout = 0
+number = ""
+
+(0..13).each do |i|
+  candidate = candidates[i.to_s].select { |c| c[1] == zout }.max_by { |c| c[0] }
+
+  zout = candidate[2]
+  number << candidate[0]
+end
+
+p number
